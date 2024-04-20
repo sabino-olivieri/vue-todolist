cda3184078108c6282499721a7a6ecbaef3fc7fc
sabino-olivieri <sabino.olivieri.28@gmail.com>
2024-04-20 12:39:26 +0200

    feat: completed up to bonus 2

--- js/script.js
+++ js/script.js
@@ -3,18 +3,34 @@ const {createApp} = Vue;
 createApp({
     data() {
         return {
+
+            newTask: {
+                text: "",
+                done: false,
+                category: "",
+            },
             
-            todoList: [
-                {
-                    text: "Quarterly Newsletter",
-                    done: true,
-                    category: "Editorial"
-                }
-            ]
+            todoList: []
 
         }
     },
     methods: {
+        addTask() {
+            if(this.newTask.text ===  "") return;
+            console.log({...this.newTask});
 
+            this.todoList.push({...this.newTask});
+
+        },
+
+        doneNotDone(index) {
+            this.todoList[index].done = !this.todoList[index].done;
+            // se è stata fatta crea una copia, lo cancella dall'array e inserisce la copia all'ultimo posto
+            if(this.todoList[index].done){
+                const lastTask = {...this.todoList[index]}
+                this.todoList.splice(index, 1)
+                this.todoList.push(lastTask);
+            }
+        }
     }
 }).mount("#app")
